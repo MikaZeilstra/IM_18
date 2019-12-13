e74c5330ecf59b352433e3264216e69ee1e3f55d
Mika Zeilstra <M.J.Zeilstra@student.tudelft.nl>
2019-12-13 11:57:05 +0100

    Add proccesing start

--- CaptureFrame_Process.py
+++ CaptureFrame_Process.py
@@ -5,6 +5,8 @@ import pandas as pd
 import Localization
 import Recognize
 
+
+THRESHOLD  = 0.725
 """
 In this file, you will define your own CaptureFrame_Process funtion. In this function,
 you need three arguments: file_path(str type, the video file), sample_frequency(second), save_path(final results saving path).
@@ -21,11 +23,27 @@ Output: None
 
 
 def CaptureFrame_Process(file_path, sample_frequency, save_path):
+    print("Now loading file " + file_path)
     cap = cv2.VideoCapture(file_path)
     ret, frame = cap.read()
 
     print(frame.shape)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    h = hsv[:,:,0]
+    hl = cv2.Laplacian(hsv,cv2.CV_64F)
+    hln = cv2.normalize(hl, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    binary = np.zeros(hln.shape[0] * hln.shape[1]).reshape(hln.shape[0:2])
+
+    print(np.max(hln))
+    for x in range(hl.shape[0]) :
+        for y in range(hl.shape[1]):
+            if(hln[x][y][0] <=THRESHOLD and hln[x][y][1] <=THRESHOLD and hln[x][y][2] <=THRESHOLD):
+                binary[x][y] = 0
+            else:
+                binary[x][y] = 1
+
+
+    cv2.imshow('frame', binary)
     cv2.waitKey()
     cap.release()
