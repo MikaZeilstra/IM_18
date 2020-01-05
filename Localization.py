27ba780f1987a84b888cf6e48efa8822765bc3ca
Alves Marinov <a.marinov@student.tudelft.nl>
2020-01-05 18:18:12 +0100

    Cleaning up the code

--- Localization.py
+++ Localization.py
@@ -17,90 +17,26 @@ Hints:
 	2. You may need to define two ways for localizing plates(yellow or other colors)
 """
 def plate_detection(image):
-	# print(frame.shape)
-	# Display the resulting frame
+
 	result = []
-	# hsv  = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	# for x in range(len(hsv)):
-	# 	for y in range(len(hsv[x])):
-	# 		pass
-	# 		#hsv[x][y][2] = 128;
-	# 		#hsv[x][y][1] = 128;
-	# lGray = cv2.Laplacian(hsv[:,:,2], cv2.CV_8U)
-	# eimage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-	# eGray = cv2.cvtColor(eimage, cv2.COLOR_BGR2GRAY)
-	# graySc = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	# gray = np.uint8(hsv[:,:,0] * 0.5 + hsv[:,:,1]*0.5)
-	# gray = hsv[:,:,1]
-	# hs = np.hstack([hsv[:,:,0] , hsv[:,:,1],hsv[:,:,2]])
+
 	rgbEdges = np.zeros(image.shape, dtype=np.uint8)
-	#edgeFuse = np.zeros(image.shape[0:2],dtype=np.uint8)
-	#print(edgeFuse.shape)
+
 	hve = np.zeros(rgbEdges[:,:,0].shape, dtype=np.uint8)
 	for plane in [0,2]:
 		rgbEdges[:,:,plane] = cv2.Canny(image[:,:,plane], 50,200)
 		hve = np.logical_or(rgbEdges[:,:,plane],hve)
-		#np.sum([rgbEdges[:,:,plane],edgeFuse], where=[True, False])
-		#np.add(edgeFuse,rgbEdges[:,:,plane],where=edgeFuse[edgeFuse==0])
-	#hse = np.hstack([rgbEdges[:, :, 0], rgbEdges[:, :, 1], rgbEdges[:, :, 2]])
-	#print(gray.shape)
-	# h = hsv[:,:,0]
+
 	hve = np.uint8(hve)
-	#hl = cv2.Laplacian(image,cv2.CV_64F)
-	# hln = np.uint8(cv2.normalize(hl, None, 0, 255, cv2.NORM_MINMAX))
-	# binary = np.zeros(hln.shape[0] * hln.shape[1]).reshape(hln.shape[0:2])
-	# ge = cv2.cvtColor(hln, cv2.COLOR_BGR2GRAY)
-	#ge = cv2.equalizeHist(graySc)
-
-
-	#t = cv2.adaptiveThreshold(graySc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 11, 0)
-	#tl = cv2.Laplacian(t,cv2.CV_8U)
-	# gray = cv2.GaussianBlur(gray,(3,3),10)
-	#gray = cv2.bilateralFilter(gray, 7, 21,51)
-	#edges = cv2.Canny(graySc, 0,200)
-	#kernel = np.ones((100, 100), np.uint8)
-	#edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
-	#edgesc = cv2.cvtColor(graySc,cv2.COLOR_GRAY2BGR)
-
-	#hve = np.uint32(np.add(rgbEdges[:, :, 0],rgbEdges[:, :, 2]))
 
 	contours, hierarchy = cv2.findContours(hve, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	#c2 = []
-	#areas = []
 
-	#mask2 = np.zeros(image[:,:,0].shape, np.uint8)
 	for i in range(len(contours)):
-		#if (hierarchy[0][i][3] < 0 and hierarchy[0][i][2] > 0):
-		#if(hierarchy[0][i][3] < 0):
+
 		if(len(contours[i]) > 3):
-			# peri = cv2.arcLength(contours[i], False)
-			# aprox = cv2.approxPolyDP(contours[i],peri * 0.02,False)
-			# if(len(aprox) == 4):
 
-
-
-
-			#recp = np.array(cv2.boxPoints(rec), dtype=np.int32)
-
-			#cv2.fillPoly(edges, [hull[:, 0, :]], 255)
-
-
-			#area = cv2.contourArea(hull)
-			# circh = cv2.arcLength(hull, True)
-			# ratio = 0
-			# try:
-			# 	ratio = area / circh
-			# except:
-			# 	pass
-			# if (250 <= len(contours[i]) <= 250):
-
-			# ------------------------
-			# Retrieve vaguely rectanglur stuff
-			# -----------------------
-			#if (len(hull) > 3):
-			#if True :
 				rec = cv2.minAreaRect(contours[i])
-				#print(rec[2])
+
 				leg_ratio = rec[1][0] / rec[1][1]
 
 				if(not (1/3 < leg_ratio < 3) and (1/5 < leg_ratio < 5)):
@@ -109,36 +45,7 @@ def plate_detection(image):
 					y_dis = np.max(recp[:, 1]) - np.min(recp[:, 1])
 					if(x_dis > 100 and y_dis > 20):
 						if(all([0 < xCor < image[:,:,0].shape[1]  for xCor in recp[:,0]]) and  all([0 < yCor < image[:,:,0].shape[0]  for yCor in recp[:,1]])):
-							#mask = np.zeros(image[:,:,0].shape, np.uint8)
-					# print(hull)
 
-					# cv2.drawContours()
-
-					# print([recp])
-
-					#print("found : " + str(i))
-
-
-							#print(cv2.contourArea(recp) )
-							#print()
-
-							#cv2.fillPoly(mask, [recp], 120)
-							#cv2.fillPoly(mask, [hull[:, 0, :]], 150)
-							#unique, counts = np.unique(mask, return_counts=True)
-
-							#print(counts[1]/counts[2])
-
-							#print(rectness)
-					#cv2.fillPoly(image, [recp], 120)
-					#cv2.fillPoly(image, [hull[:, 0, :]], 255)
-
-					# cv2.fillPoly(mask2, [recp], 120)
-					# cv2.fillPoly(mask2, [hull[:, 0, :]], 255)
-
-					#c2.append(contours[i])
-							#print(image[:,:,0].shape[1])
-							#print([xCor <= image[:,:,0].shape[1]  for xCor in recp[:,0]])
-							#print([0 <= xCor <= image[:,:,0].shape[1]  for xCor in recp[:,0]])
 							if (rec[1][0] < rec[1][1]):
 								rec = (rec[0], (rec[1][1], rec[1][0]), rec[2] + 90)
 
@@ -147,70 +54,16 @@ def plate_detection(image):
 								rectness = cv2.contourArea(hull[:, 0, :]) / cv2.contourArea(recp)
 
 								if ( rectness > 0.85 ):
-									#c2.append(contours[i])
-						#print(rec)
-						#cv2.circle(mask2, tuple(np.int32(rec[0])), 50, 255, 3)
-						#print(recp)q
-						# print(leg_ratio)
-						# print(np.linalg.norm(recp[0] - recp[1]))
-						# print(np.linalg.norm(recp[1] - recp[1]))
-						# # print(cv2.contourArea([recp]) / cv2.contourArea(cv2.UMat(hull)))
-						#print(counts)
-
-
-									#cv2.fillPoly(image, [recp], 120)
-									#cv2.fillPoly(image, [hull[:, 0, :]], 255)
-
-						# cv2.fillPoly(gray, [recp], 120)
-						# cv2.fillPoly(gray, [hull[:, 0, :]], 255)
-
-									#br = cv2.boundingRect(hull)
-									#print(recp)
-									#areas.append(br[2] * br[3])
 
 									rm = cv2.getRotationMatrix2D(tuple(rec[0]),rec[2],1)
-								#(int(np.ceil(rec[1][0])),int(np.ceil(rec[1][1])))
+
 									image = cv2.warpAffine(src=image, M=rm ,dsize=(image[:,:,0].shape[1],image[:,:,0].shape[0]))
-									#cropped = image[br[1]:br[1] + br[3], br[0]:br[0] + br[2]]
+
 									xCor = int(rec[0][0] - rec[1][0] / 2) -1
 									yCor = int(rec[0][1] - rec[1][1] / 2) -1
-									#cropped = image[0: hve.shape[0], 0:hve.shape[1]]
+
 									cropped = image[yCor: yCor + int(rec[1][1])+ 2,  xCor:xCor + int(rec[1][0]) + 2]
 
-									#cv2.imshow("",image)
-									#cv2.waitKey()
-
 									result.append(cropped)
-								#return cropped
 
-			# defects = cv2.convexityDefects(contours[i], hulld)
-			# defect_sum = np.sum(defects[:,0,3]/256)
-			# print(defects)
-			# print(defect_sum)
-			# print(len(hull ))
-			# cv2.rectangle(tc, (rec[0], rec[1]), (rec[0] + rec[2], rec[1] + rec[3]), 170, 2)
-	# print(len(c2))
-
-	# rec = cv2.boundingRect(c2[0])
-
-	# print("Area : " + str(rec[2] * rec[3]))
-	# print("circ : " + str(rec[2] * 2 + 2* rec[3]))
-	# print((rec[2] * rec[3])/(rec[2] * 2 + 2 * rec[3]))
-	#areaS = np.argsort(areas)
-
-	#c3 = []
-
-	#for i in range(0,4) :
-	#	c3.append(c2[areaS[i]])
-
-	#cv2.drawContours(mask2 ,c2, -1, 128,1)
-	#plateTemplate = cv2.imread("TrainingSet/Templates/BinTemplate.jpg", cv2.IMREAD_GRAYSCALE)
-	# gray_edge = np.hstack((gray,edges))
 	return result
-
-# alg = cv2.createGeneralizedHoughBallard()
-# alg.setTemplate(plateTemplate)
-# pos, votes = alg.detect(tq)
-# print(pos[0][0][0:2])
-# print(votes.shape)
-#
--- Recognize.py
+++ Recognize.py
@@ -21,16 +21,6 @@ Hints:
 
 
 def segment_and_recognize(plate_imgs, trIm):
-    # print(plate_imgs)
-    # cv2.namedWindow("pl" , cv2.WINDOW_NORMAL)
-    # cv2.imshow("pl", plate_imgs)
-    # cv2.waitKey()
-
-    # Initiate SIFT detector
-    # kp2 = []
-    # des2 = []
-
-    # print(trIm)
     dashes = False
 
     gray = cv2.cvtColor(plate_imgs, cv2.COLOR_BGR2GRAY)
@@ -38,85 +28,50 @@ def segment_and_recognize(plate_imgs, trIm):
         gray = cv2.bilateralFilter(gray, -1, 10, 11)
 
     t = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 5)
-    # print(np.average(gray))
-    # gray = cv2.bilateralFilter(gray, 7, 21, 51)q
-
-    #
-    # img3 = t
-
-    # kernel = np.ones((2, 2), np.uint8)
-    # t = cv2.morphologyEx(t, cv2.MORPH_OPEN, kernel,iterations=1)
-    # t = cv2.morphologyEx(t, cv2.MORPH_CLOSE, kernel,iterations=1)
-    # newT = cv2.Canny(gray, 100, 200)
-
     newT = cv2.Laplacian(t, cv2.CV_8U)
 
-    # cv2.imshow("T", newT)
-    # cv2.waitKey()
-
-    # edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones([2, 2]), iterations=2)
 
     cont, hier = cv2.findContours(newT, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    # mask = np.zeros(plate_imgs.shape)
-    # c2 = []
     checked = []
     brecs = {}
     dbrecs = {}
     contIds = []
-    # areadict = {}
-    # area = []
 
     brecVals = []
     plate = []
     dashid = []
     for i in range(len(cont)):
         if (len(cont[i]) > 3):
-            # minRec = cv2.minAreaRect(cont[i])
-            # hull = cv2.convexHull(cont[i])
             brec = cv2.boundingRect(cont[i])
 
             if (brec[2] < brec[3] and brec[2] > 5 and brec[3] > 10 and [brec[0], brec[1]] not in checked):
-                #cv2.rectangle(newT, (brec[0], brec[1]), (brec[0] + brec[2], brec[1] + brec[3]), 120, 1)
+
                 checked.append([brec[0], brec[1]])
                 brecs[i] = brec
                 contIds.append(i)
 
-            # cv2.circle(edges, (brec[0],brec[1]), 10, 170, 2)
-            # mask = np.zeros(edges.shape)
-            # cv2.drawContours(mask,cont,i,255)
-            # print()
-            # cv2.imshow("", mask)
-            # cv2.waitKey()
             elif (brec[2] < plate_imgs.shape[1] / 3 and brec[2] < plate_imgs.shape[0] / 3 and 1.1 > brec[3] / brec[2] > 1 / 3.5 and [brec[0], brec[1]] not in checked):
                 checked.append([brec[0], brec[1]])
                 dashid.append(i)
                 dbrecs[i] = brec
-    # children = []
-    # childMap = defaultdict(list)
+
     for id in contIds[:]:
-        # print(hier[0][id][2])
-        # print(id)
+
         isInside = check_inside(brecs, brecs[id], id)
         if isInside:
-            # children.append(cont[id])
-            # childMap[isInside - 1].append(cont[id])
             contIds.remove(id)
             del brecs[id]
-    # 		#print("t")
-    # 		#cv2.drawContours(newT, cont, id, 90, -1)
+
 
     if (len(brecs) > 0):
-        # print(np.array(list(brecs.values()))[:, 1])
-        #print(contIds)
+
         mode_Y = st.mode(np.array(list(brecs.values()))[:, 1])[0]
         mode_High = st.mode(np.array(list(brecs.values()))[:, 3])[0]
         eps = np.ceil(plate_imgs.shape[0] / 10)
 
-        #print(mode_Y)
-        # print(eps)
 
         for id in contIds[:]:
-            #cv2.rectangle(newT, (brecs[id][0], brecs[id][1]),(brecs[id][0] + brecs[id][2], brecs[id][1] + brecs[id][3]), 120, 1)
+
             if not (mode_Y - eps <  brecs[id][1] < mode_Y + eps and mode_High - eps < brecs[id][3] < mode_High + eps):
                 contIds.remove(id)
                 del brecs[id]
@@ -130,23 +85,16 @@ def segment_and_recognize(plate_imgs, trIm):
     for id in dashid:
         croppedImage = newT[dbrecs[id][1]:dbrecs[id][1] + dbrecs[id][3], dbrecs[id][0]:dbrecs[id][0] + dbrecs[id][2]].copy()
         cv2.floodFill(croppedImage, None, (int(croppedImage.shape[1] / 2) - 1, int(croppedImage.shape[0] / 2) - 1), 255)
-        # print((int(croppedImage.shape[1]/2),int(croppedImage.shape[0]/2)))
-        # cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-        # cv2.imshow("frame", croppedImage)
-        # cv2.waitKey()
         if (np.average(croppedImage) > 0.6 * 255):
             dashes = True
-        # print(dashes)
+
 
     for id in contIds:
 
         allDiffs = []
 
-        #cv2.rectangle(newT, (brecs[id][0], brecs[id][1]),(brecs[id][0] + brecs[id][2], brecs[id][1] + brecs[id][3]), 120, 1)
-        # print(brecs[id][1])
-        # for t in range(0, 10):
         croppedImage = newT[brecs[id][1]:brecs[id][1] + brecs[id][3], brecs[id][0]:brecs[id][0] + brecs[id][2]]
-        # croppedImage = cv2.copyMakeBorder(croppedImage, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
+
 
         ccontours, chier = cv2.findContours(croppedImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -160,7 +108,6 @@ def segment_and_recognize(plate_imgs, trIm):
                 cropchecked.append([cbrec[0], cbrec[1]])
                 cbrecs[ci] = cbrec
                 ccontIds.append(ci)
-            # cv2.rectangle(croppedImage, (cbrecs[ci][0], cbrecs[ci][1]), (cbrecs[ci][0] + cbrecs[ci][2], cbrecs[ci][1] + cbrecs[ci][3]), 120, 1)
 
         cropchildMap = defaultdict(list)
 
@@ -172,19 +119,10 @@ def segment_and_recognize(plate_imgs, trIm):
                 ccontIds.remove(cid)
                 del cbrecs[cid]
 
-        # symbol = np.zeros(croppedImage.shape)
         for cid in ccontIds:
-            # pass
-
             cv2.drawContours(croppedImage, ccontours, cid, 255, -1)
             cv2.drawContours(croppedImage, cropchildMap[cid], -1, 0, -1)
 
-        # print(len(cropchildMap[ccontIds[0]]))
-
-        # mask = np.zeros(croppedImage.shape)
-
-        # cv2.drawContours(newT, cont, id, 255, -1)
-        # cv2.drawContours(newT, childMap[id], -1,0,-1)
         croppedImage = cv2.morphologyEx(croppedImage, cv2.MORPH_OPEN, np.ones([2, 2]), iterations=1)
 
         width = int(croppedImage.shape[1])
@@ -193,11 +131,7 @@ def segment_and_recognize(plate_imgs, trIm):
 
         for t in range(0, 27):
             image = trIm[t]
-
-            # resize image
             resized = cv2.resize(image, dim, interpolation=cv2.INTER_NEAREST)
-
-            # print("SHAPE RESIZED : " + str(resized.shape[0]))
 
             diff = np.logical_xor(resized, croppedImage, dtype=np.int16)
 
@@ -205,159 +139,38 @@ def segment_and_recognize(plate_imgs, trIm):
 
             allDiffs.append(diff)
 
-        # print("COMPARING WITH " + str(t) + " AND DIFF IS: " + str(diff))
-        # print("DIFFERENCE : " + str(diff))
-        # cv2.namedWindow("L", cv2.WINDOW_NORMAL)
-        # cv2.imshow("L", np.hstack([croppedImage, resized]))
-        # cv2.waitKey()
-
-        #cv2.namedWindow("f",cv2.WINDOW_NORMAL)
-        #cv2.imshow("f", croppedImage)
-        #cv2.waitKey()
-        #
         minIndex = np.argmin(allDiffs)
 
-        # print("found possible " + str(minIndex) + " with diff " + str(allDiffs[minIndex]))
         if (allDiffs[minIndex] < 0.35):
-            # pass
-            # print("WE FOUND A :" + str(minIndex))
+
             brecVals.append(brecs[id][0])
-            # print("APPENDING: " + str(brec[0]))
+
             plate.append(minIndex)
 
     finalPlate = []
 
-    #cv2.namedWindow("newT", cv2.WINDOW_NORMAL)
-    #cv2.imshow("newT", newT)
-    #cv2.waitKey()
-
-    # print(brecVals)
 
     valsLength = len(brecVals)
 
     sortedMin = np.argsort(brecVals)
 
-    # print(sortedMin)
 
     for i in range(0, valsLength):
-        # minX = np.argmin(brecVals)
-
         finalPlate.append(plate[sortedMin[i]])
-    # brecVals.remove(brecVals[minX])
-    # plate.remove(plate[minX])
-
-    # print("LENGTH OF THE PLATE IS: " +str(len(finalPlate)))
     if len(finalPlate) >= 6:
-        # print("PRINTINT LICENTE PLATE : ----------------------------")
-
-         #print(finalPlate)
-        # print(dashes)
         if dashes:
             finalPlate.append(27)
 
-        # print(ffinalPlate)
         return tuple(finalPlate)
     else:
         return []
 
 
-# cv2.drawContours(plate_imgs,c2, -1, 255, 1)
-# cv2.imshow('frame', edges)
-# cv2.imshow("frame", img3)
-# cv2.waitKey()
-
-# return finalPlate
 
 def check_inside(br_list, check, br_id):
-    # print(br_list)
     for id, br in br_list.items():
         if check[0] >= br[0] and check[1] >= br[1] and check[0] + check[2] <= br[0] + br[2] and check[1] + check[3] <= \
                 br[3] + br[3] and br_id != id:
             return id + 1
     return False
 
-
-#
-# 		for t in range(0, 27):
-# 			image = trIm[t]
-#
-# 			# resize image
-# 			resized = cv2.resize(image, dim, interpolation=cv2.INTER_NEAREST)
-#
-# 			# print("SHAPE RESIZED : " + str(resized.shape[0]))
-#
-# 			diff = np.logical_xor(resized, croppedImage, dtype=np.int16)
-#
-# 			diff = np.sum(diff) / (croppedImage.shape[0] * croppedImage.shape[1])
-#
-# 			allDiffs.append(diff)
-#
-# 			#print("COMPARING WITH " + str(t) + " AND DIFF IS: " + str(diff))
-# 			#print("DIFFERENCE : " + str(diff))
-# 			#cv2.namedWindow("L", cv2.WINDOW_NORMAL)
-# 			#cv2.imshow("L", np.hstack([croppedImage, resized]))
-# 			#cv2.waitKey()
-#
-# 		#cv2.namedWindow("f",cv2.WINDOW_NORMAL)
-# 		#cv2.imshow("f", croppedImage)
-# 		#cv2.waitKey()
-# #
-# 		minIndex = np.argmin(allDiffs)
-#
-# 		#print("found possible " + str(minIndex) + " with diff " + str(allDiffs[minIndex]))
-# 		if (allDiffs[minIndex] < 0.35):
-# 			#pass
-# 			#print("WE FOUND A :" + str(minIndex))
-# 			brecVals.append(brecs[id][0])
-# 			# print("APPENDING: " + str(brec[0]))
-# 			plate.append(minIndex)
-#
-#
-# 	finalPlate = []
-#
-# 	cv2.namedWindow("newT", cv2.WINDOW_NORMAL)
-# 	cv2.imshow("newT", newT)
-# 	#cv2.waitKey()
-#
-# 	#print(brecVals)
-#
-#
-#
-#
-# 	valsLength = len(brecVals)
-#
-# 	sortedMin = np.argsort(brecVals)
-#
-# 	#print(sortedMin)
-#
-# 	for i in range(0, valsLength) :
-# 		#minX = np.argmin(brecVals)
-#
-# 		finalPlate.append(plate[sortedMin[i]])
-# 		#brecVals.remove(brecVals[minX])
-# 		#plate.remove(plate[minX])
-#
-#
-# 	#print("LENGTH OF THE PLATE IS: " +str(len(finalPlate)))
-# 	if len(finalPlate) >= 6:
-# 		#print("PRINTINT LICENTE PLATE : ----------------------------")
-#
-# 		#print(finalPlate)
-#
-# 		return tuple(finalPlate)
-# 	else:
-# 		return []
-#
-# 	#cv2.drawContours(plate_imgs,c2, -1, 255, 1)
-# 	#cv2.imshow('frame', edges)
-# 	#cv2.imshow("frame", img3)
-# 	#cv2.waitKey()
-#
-# 	#return finalPlate
-#
-# def check_inside(br_list,check, br_id):
-# 	#print(br_list)
-# 	for id , br in br_list.items():
-# 		if check[0] >= br[0] and check[1] >= br[1] and check[0] + check[2] <= br[0] + br[2] and check[1] + check[3] <= br[3] + br[3] and br_id != id:
-# 			return id + 1
-# 	return False
